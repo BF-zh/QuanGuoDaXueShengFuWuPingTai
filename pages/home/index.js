e34c0acc1b8ac76f79dae541dee62436533456c8
北风 <2581807417@qq.com>
2023-06-07 08:23:14 +0800

    完成了主页

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    liveRoomList:[
+      {
+        title:"易展翅525人才节",
+        src:"/static/521rcj.png",
+        time:"05-25 10:00",
+        count:"10.10万"
+      },
+      {
+        title:"2023春招冲刺·运营行业急招",
+        src:"/static/2023czcc.png",
+        time:"04-12 19:00",
+        count:"3368"
+      },
+      {
+        title:"2023春招冲刺·热门高薪职位",
+        src:"/static/2023czccHOT.jpeg",
+        time:"04-12 19:00",
+        count:"3368"
+      },
+      {
+        title:"2023春招冲刺·500强行业龙头专场",
+        src:"/static/2023czccTOP500.jpeg",
+        time:"04-24 17:00",
+        count:"3991"
+      },
+      {
+        title:"秒懂求职-冲刺金三银四",
+        src:"/static/mdqz.png",
+        time:"04-06 15:00",
+        count:"4229"
+      },
+      {
+        title:`"秦青展翅"[阎良区空天产业人才发展合作论坛]直播`,
+        src:"/static/zhktzxrc.jpeg",
+        time:"03-10 09:30",
+        count:"4.52万"
+      },
+      {
+        title:`2023春招进行时-有问必答`,
+        src:"/static/2023Spring.png",
+        time:"03-03 17:30",
+        count:"4765"
+      },
+      {
+        title:`2023春招进行时-热门岗位急招`,
+        src:"/static/2023Springing.jpeg",
+        time:"03-06 17:30",
+        count:"7187"
+      },
+    ],
+    tabs:{
+      activeId:0,
+      tab:[
+        {
+          title:"推荐",
+          id:0
+        },
+        {
+          title:"最新",
+          id:1
+        }
+      ]
+    }
   },
 
   /**
